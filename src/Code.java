641a2ff18876cb9914ec20edd62214dbae60a393
initial commit <polina.ostrovskaya.2003@mail.ru>
2023-10-09 20:54:47 +0300

    Two commit(branch)

--- src/Code.java
+++ src/Code.java
@@ -4,22 +4,14 @@ public class Code {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n= scanner.nextInt();
-        if(n == 5)
-        {
-            System.out.println("HI");
-        }else
+        if(n<=10)
         {
             System.out.println("BYE");
+        }else
+        {
+            System.out.println("HI");
         }
         int a=n-1;
-        if(a>2)
-        {
-            System.out.println("Good");
-        }
-        else
-        {
-            System.out.println("Bad");
-        }
     }
 }
 
